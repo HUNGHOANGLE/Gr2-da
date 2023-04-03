689179b725be5dbc0789bfbf0ebe86146a036995
LeHuuHoangHung <hunghuu3010@gmail.com>
2023-04-03 18:25:40 +0700

    Removed Previous Commits

---
+++ ecommerce-api/app/config/index.js
@@ -0,0 +1,21 @@
+const config = {
+  database: {
+    connection: "postgres://postgres:12345678@localhost:5432/gr2",
+  },
+  api: {
+    host: "localhost",
+    port: 8080,
+  },
+  jwt: {
+    secretKey: "secretKey123",
+  },
+  sendgrid: {
+    secretKey: null,
+    fromEmail: null,
+  },
+  webClient: {
+    url: "http://localhost:3007",
+  },
+};
+
+module.exports = config;
